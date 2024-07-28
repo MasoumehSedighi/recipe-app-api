0ac69dc155460a5f08cac696dd4280ca823672e7
MasoumehSedighi <mas_cutt4@yahoo.com>
2024-07-28 16:18:38 +1000

    Add recipe APIs

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -58,7 +58,7 @@ class ModelTest(TestCase):
         recipe = models.Recipe.objects.create(
             user=user,
             title='Sample recipe name',
-            time_minute=5,
+            time_minutes=5,
             price=Decimal('5.50'),
             description='Sample recipe description.'
         )
