c83f3a49993c716d2a0582995f749eea549ab6be
MasoumehSedighi <mas_cutt4@yahoo.com>
2024-08-03 19:28:43 +1000

    Add filtering

--- app/app/settings.py
+++ app/app/settings.py
@@ -127,8 +127,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-
-#New
 STATIC_URL = '/static/static/'
 MEDIA_URL = '/static/media/'
 
@@ -139,7 +137,6 @@ STATIC_ROOT = 'vol/web/staric'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-#New
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
